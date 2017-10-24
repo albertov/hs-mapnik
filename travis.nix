fb3c1a2fbaa3791e98a607829f48e46d4621380c
Alberto Valverde <alberto@toscat.net>
2017-10-24 19:46:25 +0200

    pin nixpkgs version and travis-ci for osx

--- travis.nix
+++ travis.nix
@@ -1,7 +1,11 @@
-{ system ? builtins.currentSystem }:
+{ system ? builtins.currentSystem
+}:
 
 let
-  pkgs = import ./nixpkgs { inherit system; };
+  revision = "36a4dc392a40e06973a64b7666b179f4a021682a";
+  nixpkgsUrl =
+    "https://github.com/NixOS/nixpkgs-channels/archive/${revision}.tar.gz";
+  pkgs = import (fetchTarball nixpkgsUrl) {};
   withGhc = ghc:
     let myGhc = ghc.override {
       overrides = self: super:
@@ -11,6 +15,7 @@ let
       hs-mapnik
       ]);
 in {
-  with_801 = withGhc pkgs.haskell.packages.ghc801;
+  with_802 = withGhc pkgs.haskell.packages.ghc802;
   with_710 = withGhc pkgs.haskell.packages.ghc710;
+  with_821 = withGhc pkgs.haskell.packages.ghc821;
 }
