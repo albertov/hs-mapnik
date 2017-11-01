872f4654d3dd9db3ded423035ca36db90ba59a16
Alberto Valverde <alberto@toscat.net>
2017-11-01 19:19:44 +0100

    travis fix attempt

--- travis.nix
+++ travis.nix
@@ -9,12 +9,15 @@ let
   withGhc = ghc:
     let myGhc = ghc.override {
       overrides = self: super:
-        { hs-mapnik = self.callPackage ./.  { inherit (pkgs) mapnik; }; };
+        {
+          hs-mapnik = self.callPackage ./.  { inherit (pkgs) mapnik; };
+          inline-c = super.inline-c_0_6_0_5;
+          inline-c-cpp = super.inline-c-cpp_0_2_1_0;
+        };
       };
     in myGhc.ghcWithPackages (haskellPackages: with haskellPackages; [
       hs-mapnik
       ]);
 in {
-  with_802 = withGhc pkgs.haskell.packages.ghc802;
   with_821 = withGhc pkgs.haskell.packages.ghc821;
 }
