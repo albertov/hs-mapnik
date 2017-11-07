2c7f79b7b0f01d219b8d40f5e3777ad7f29a4331
Alberto Valverde <alberto@toscat.net>
2017-11-07 10:12:06 +0100

    Extracted cabal setup hook into separate package so it can be reused to ease compiling against mapnik

--- hs-mapnik-bindings/default.nix
+++ hs-mapnik-bindings/default.nix
@@ -1,7 +1,7 @@
 { mkDerivation, fetchFromGitHub, base, Cabal, directory
 , filepath, hspec, hspec-core, process, stdenv
 , bytestring, icu, harfbuzz, mapnik, boost, inline-c-cpp
-, inline-c, hs-mapnik
+, inline-c, hs-mapnik, hs-mapnik-setup, text
 }:
 mkDerivation {
   pname = "hs-mapnik-bindings";
@@ -9,7 +9,7 @@ mkDerivation {
   src = ./.;
   preConfigure = "export MAPNIK_CONFIG=${mapnik}/bin/mapnik-config";
   setupHaskellDepends = [ base Cabal directory filepath process ];
-  libraryHaskellDepends = [ base bytestring inline-c-cpp inline-c hs-mapnik ];
+  libraryHaskellDepends = [ base bytestring inline-c-cpp inline-c hs-mapnik hs-mapnik-setup ];
   librarySystemDepends = [ mapnik boost.dev icu.dev harfbuzz.dev ];
   testHaskellDepends = [
     base bytestring hspec hspec-core text hs-mapnik
