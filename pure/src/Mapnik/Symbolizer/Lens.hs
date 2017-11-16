0a59168891dad76ed1f7b21591a72c7351d26cc7
Alberto Valverde <alberto@toscat.net>
2017-11-16 21:06:59 +0100

    AppLens -> Traversal

--- pure/src/Mapnik/Symbolizer/Lens.hs
+++ pure/src/Mapnik/Symbolizer/Lens.hs
@@ -10,14 +10,12 @@ import Mapnik.Symbolizer
 
 import Control.Lens
 
-type ApLens' s a = forall f . Applicative f => (a -> f a) -> s -> f s
-
-class HasAllowOverlap s a | s -> a where allowOverlap :: ApLens' s a
-class HasFill s a | s -> a where fill :: ApLens' s a
-class HasAvoidEdges s a | s -> a where avoidEdges :: ApLens' s a
-class HasDirection s a | s -> a where direction :: ApLens' s a
-class HasDx s a | s -> a where dx :: ApLens' s a
-class HasDy s a | s -> a where dy :: ApLens' s a
+class HasAllowOverlap s a | s -> a where allowOverlap :: Traversal' s a
+class HasFill         s a | s -> a where fill         :: Traversal' s a
+class HasAvoidEdges   s a | s -> a where avoidEdges   :: Traversal' s a
+class HasDirection    s a | s -> a where direction    :: Traversal' s a
+class HasDx           s a | s -> a where dx           :: Traversal' s a
+class HasDy           s a | s -> a where dy           :: Traversal' s a
 
 makeMapnikFields ''Format
 makeMapnikFields ''TextProperties
